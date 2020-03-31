1e050e6e7b743a3767c72841fcd86ded8f120f95
mlarmet <maxence22@outlook.com>
2020-03-31 20:16:46 +0200

    update effect js

--- js/animation.js
+++ js/animation.js
@@ -6,22 +6,26 @@ jQuery(function ($) {
         $("#firstname").css("padding-right", "75%");
         $("#name").css("padding-left", "75%");
         $(".bg-img").css("filter", "blur(30px)");
-
+        $(".title h1").css("opacity", 0);
         setTimeout(function () {
-
+            
             $({ value: 30 }).animate({ value: 0 }, {
                 duration: 750,
                 easing: 'swing',
                 step: function () {
                     console.log(this.value);
-                    $('.bg-img').css({
+                    $(".bg-img, .title h1").css({
                         "filter": "blur(" + this.value + "px)"
+                    });
+                    $(".title h1").css({
+                        "opacity": (30 - this.value)/30
                     });
                 }
             });
             
+
             $('.title h1').animate({
-                'padding': '0 25%'
+                'padding': '0 25%' 
             }, 1250);
 
         }, 500);
