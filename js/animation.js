49dcf747457f9cb94c9fa1e64f9576cd1ddb375d
mlarmet <maxence22@outlook.com>
2020-04-02 00:14:32 +0200

    update profil info

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,10 @@
 jQuery(function ($) {
 
-    var ready;        
-    ready = false;
-
     $(window).on("load", function () {
 
-
+        //attend pour que la page soit bien chargé
         setTimeout(function () {
-                        
+                                 
             $({ value: 30 }).animate({ value: 0 }, {
                 duration: 750,
                 step: function () {
@@ -24,39 +21,32 @@ jQuery(function ($) {
                 'padding': '0 25%'
             }, 1250);
 
-            
-            ready = true;
         }, 500);
-        
-
-            
-        $("#firstname, #name").mouseenter(function () {
-            if (ready){
-                $('#firstname').stop(true, false)
-                $('#firstname').animate({
-                    'padding-left': '12.5%'
+  
+        //attend la fin de l'animation du début 
+        setTimeout(function () {   
+            $("#title").mouseenter(function () {
+                $('#slide-top').stop(true, false)
+                $('#slide-top').animate({
+                    'padding-left': '21.875%'
                 }, 500);
 
-                $('#name').stop(true, false)
-                $('#name').animate({
-                    'padding-right': '12.5%'
+                $('#slide-bottom').stop(true, false)
+                $('#slide-bottom').animate({
+                    'padding-right': '21.875%'
                 }, 500);
-            }
 
-        });
+            });
 
-        $("#firstname, #name").mouseleave(function () {
-            if (ready) {
+            $("#title").mouseleave(function () {
                 $('#title h1').stop(true, false)
                 $('#title h1').animate({
                     'padding': '0 25%'
                 }, 500);
 
-            }
-            else {
-                console.log(ready);
-            }
-        });
+            });
+
+        }, 1750);
         
     });
 
