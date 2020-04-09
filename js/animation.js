30545e80b9a209726614906bf42bfe0a0bb3f10c
mlarmet <maxence22@outlook.com>
2020-04-10 00:12:32 +0200

    create RWD

--- js/animation.js
+++ js/animation.js
@@ -1,110 +1,179 @@
 var scrollLimit = 50;
-var move = 1;
 
-function loop() {
+var isMobile = navigator.userAgent.toLowerCase().match(/mobile/i);
+
+var previousW = $(window).width();
+
+jQuery(function () {
     
-    $('#arrow').css({ 'bottom': (move*10) + 'px' });
-    move = 1- move;
-}
+    //alert($(window).width());
+    $(window).resize(function () {
+        var w = $(window).width();
+        console.log("w=" + w);
+        console.log("previous=" + previousW);
 
-$(document).ready(function(){
-    if (document.body.scrollTop < scrollLimit ||
-        document.documentElement.scrollTop < scrollLimit) {
-        $(".navbar").slideUp(0);
-    }
+        if (previousW < 700 && w > 700 || previousW > 700 && w < 700) {
+            location.reload();
+            previousW = w;
+        }
 
-    setInterval(loop, 500);
-  
-});
-    
-$(window).scroll(function (event) {
-    if (document.body.scrollTop > scrollLimit || 
-        document.documentElement.scrollTop > scrollLimit){
-        $(".navbar").slideDown();
-    } else {
-        $(".navbar").slideUp();
-    }
-});
+        //reset mouse hover action (only for large devices)
+        else if (previousW < 1080 && w > 1080 || previousW > 1080 && w < 1080) {
+            location.reload();
+            previousW = w;
+        }
+        
 
-$(window).on("load", function () {
-
-    $(function () {
-
-        var $img = $('header img'),
-            totalImg = $img.length;
-        var waitImgDone = function () {
-            totalImg--;
-        };
-
-        $('header img').each(function () {
-            $(this)
-                .load(waitImgDone)
-                .error(waitImgDone);
-        });
     });
 
-    //scroll up refresh page
-    $("html, body").animate({ scrollTop: 0 }, 0);
+    $(document).ready(function () {
+        if (document.body.scrollTop < scrollLimit ||
+            document.documentElement.scrollTop < scrollLimit) {
+            $(".navbar").slideUp(0);
+        }
+    });
 
-    //attend pour que la page soit bien chargé
-    setTimeout(function () {
-
-        $({ value: 30 }).animate({ value: 0 }, {
-            duration: 750,
-            step: function () {
-                $("header, #title h1").css({
-                    "filter": "blur(" + this.value + "px)",
-                    "-moz-filter": "blur(" + this.value + "px)"
-                });
-                $("#title h1").css({
-                    "opacity": (30 - this.value) / 30
-                });
+    
+        
+        $(window).scroll(function (event) {
+            if (window.matchMedia("(min-width: 700px)").matches){
+                if (document.body.scrollTop > scrollLimit ||
+                    document.documentElement.scrollTop > scrollLimit) {
+                    $(".navbar").slideDown();
+                } else {
+                    $(".navbar").slideUp();
+                }
             }
         });
+    
 
-        $('#title h1').animate({
-            'padding': '0 25%'
-        }, 1300);
-    }, 750);
-    //attend la fin de l'animation du début 
-    setTimeout(function () {
 
-        //affiche le titre au cas où l'animation aurait ratée
-        $("header, #title h1").css({"filter": "blur(" + 0 + "px)"});
-        $("#title h1").css({"opacity": 1});
-        $('#title h1').css({'padding': '0 25%'});
-        $("html").css({ 'overflow-y': 'visible' });
-        
-        //effet titre souris passe dessus
-        $("#title").mouseenter(function () {
-            $('#slide-top').stop(true, false)
-            $('#slide-top').animate({
-                'padding-left': '21.875%'
-            }, 500);
+    $(window).on("load", function () {
 
-            $('#slide-bottom').stop(true, false);
-            $('#slide-bottom').animate({
-                'padding-right': '21.875%'
-            }, 500);
-        }).mouseleave(function () {
-            $('#title h1').stop(true, false);
-            $('#title h1').animate({
-                'padding': '0 25%'
-            }, 500);
+        w = $(document).width();
 
+        $(function () {
+
+            var $img = $('header img'),
+                totalImg = $img.length;
+            var waitImgDone = function () {
+                totalImg--;
+            };
+
+            $('header img').each(function () {
+                $(this)
+                    .load(waitImgDone)
+                    .error(waitImgDone);
+            });
         });
-    }, 2005);
+
+        //scroll up refresh page
+        //$("html, body").animate({ scrollTop: 0 }, 0);
 
 
-    //souris sur le mail (attention le "a" pas le "p")
-    $("#mail a").mouseenter(function () {
-        $('#mail').animate({
-            'width': '25em'
-        }, 300);
-    }).mouseleave(function () {
-        $('#mail').animate({
-            'width': '10.25em'
-        }, 300);
+        //attend pour que la page soit bien chargé
+        setTimeout(function () {
+            $({ value: 30 }).animate({ value: 0 }, {
+                duration: 750,
+                step: function () {
+                    $("header, #title h1").css({
+                        "filter": "blur(" + this.value + "px)",
+                        "-moz-filter": "blur(" + this.value + "px)"
+                    });
+                    $("#title h1, #arrow").css({
+                        "opacity": (30 - this.value) / 30
+                    });
+                }
+            });
+
+        }, 750);
+
+        setTimeout(function () {
+            //affiche le titre au cas où l'animation aurait ratée
+            $("header, #title h1").css({ "filter": "blur(" + 0 + "px)" });
+            $("#title h1, #arrow").css({ "opacity": 1 });
+            $("html").css({ 'overflow-y': 'visible' });
+        }, 2005);
+
+        
+        function sizeChange() {
+            
+        }
+        //attend pour que la page soit bien chargé
+        if (!isMobile){
+            
+            if (window.matchMedia("(min-width: 1080px)").matches) {
+                setTimeout(function () {
+                    $('#slide-top').css({
+                        'padding-right': '50%'
+                    });
+                    $('#slide-bottom').css({
+                        'padding-left': '50%'
+                    });
+
+                    $('#title h1').animate({
+                        'padding': '0 25%'
+                    }, 1300);
+                }, 750);
+                
+                //attend la fin de l'animation du début 
+                setTimeout(function () {
+                    $('#title h1').css({ 'padding': '0 25%' });
+
+                    //effet titre souris passe dessus
+                    $("#title h1").mouseenter(function () {
+                        $('#slide-top').stop(true, false)
+                        $('#slide-top').animate({
+                            'padding-left': '21.875%'
+                        }, 500);
+
+                        $('#slide-bottom').stop(true, false);
+                        $('#slide-bottom').animate({
+                            'padding-right': '21.875%'
+                        }, 500);
+                    }).mouseleave(function () {
+                        $('#title h1').stop(true, false);
+                        $('#title h1').animate({
+                            'padding': '0 25%'
+                        }, 500);
+                    }); 
+                }, 2005); 
+            }
+            if (window.matchMedia("(min-width: 700px)").matches){
+                //souris sur le mail (attention le "a" pas le "p")
+                $("#mail a").mouseenter(function () {
+                    $('#mail').animate({
+                        'width': '22em'
+                    }, 300);
+
+                }).mouseleave(function () {
+                    $('#mail').animate({
+                        'width': '10.25em'
+                    }, 300);
+                });
+            }
+        
+        }
+
+        if(isMobile) {
+            $("header, #title h1").css({ "filter": "blur(" + 0 + "px)" });
+            $("#title h1, #arrow").css({ "opacity": 1 });            
+            $("html").css({ 'overflow-y': 'visible' });
+        }
+        
+
     });
+    // $(windows).resize(function () {
+    //     if (window.matchMedia("(max-width:699px)").matches){
+
+    //         $("header, #title h1").css({ "filter": "blur(" + 0 + "px)" });
+    //         $("#title h1, #arrow").css({ "opacity": 1 });
+    //         $('#title h1').css({ 'padding': '0 25%' });
+    //         $("html").css({ 'overflow-y': 'visible' });
+
+    //         $(".navbar").slideUp(0);
+    //     }
+
+    // });
 
 });
